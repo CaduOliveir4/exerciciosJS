75a0739bcfa1318e2f0dfc073bf2baaff218435e
Carlos Oliveira <Cadu4liffe@gmail.com>
2023-05-19 06:20:28 -0300

    atualização pós curso de algoritimos

--- ARRAYS/testeDeCodigo.js
+++ ARRAYS/testeDeCodigo.js
@@ -1,7 +1,8 @@
-let num1 = 10;
-let num2 = num1;
+function ola(){
+    function mundo() {
+        console.trace('Ola Mundo');
+      }
+  mundo();
+}
 
-num2 += 5;
-num1 += 1;
-
-console.log(`Num1 é ${num1}. Num2 é ${num2}`);
+ola();
